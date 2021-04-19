3c497d506d998d8bd5d1b6c8512492cebb027696
clairecassidy <clairecassidy0@hotmail.com>
2021-04-19 15:41:42 +0100

    add login logic

--- CloudGroupClient.py
+++ CloudGroupClient.py
@@ -4,14 +4,19 @@ from multiprocessing.connection import Client
 
 USERS_PATH = r"cam_files\users"
 ADMINS_PATH = r"cam_files\admins"
+USER_PRIVILEGE = 0
+ADMIN_PRIVILEGE = 1
 
 def main():
     # address = ('localhost', 6000)
     # conn = Client(address, authkey=b'cloud_group')
 
     while True:
-        user_list = load_usernames([USERS_PATH, ADMINS_PATH])
-        username, privilege_lvl = prompt_for_login()
+        users, admins = load_usernames(USERS_PATH, ADMINS_PATH)
+        username, privilege_lvl = prompt_for_login(users, admins)
+
+        print(f'Welcome {username}')
+
 
         # # msg = input('Send a message to CAM (\'close\' to terminate): ')
         # filename = input('Hello user1. Please give the name of the file you wish to encrypt: ')
@@ -43,26 +48,74 @@ def main():
         #
         # print(f'\n')
 
-def load_usernames(paths_to_search):
 
-    usernames = []
+def load_usernames(users_path, admins_path):
 
-    for directory_path in paths_to_search:
-        # Get the current directory
-        cur_dir = os.path.dirname(os.path.realpath(__file__))
-        # Get absolute path to folders containing user info
-        full_path = os.path.join(cur_dir, directory_path)
-        # Get all files in the directory, remove their extensions and add them to the list of usernames
-        usernames = usernames + [os.path.splitext(file)[0] for file in os.listdir(full_path)]
+    users = []
+    admins = []
 
-    print(f'{usernames}')
-    return usernames
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+
+    full_path_users = os.path.join(cur_dir, users_path)
+    users = [os.path.splitext(file)[0] for file in os.listdir(full_path_users)]
+
+    full_path_admins = os.path.join(cur_dir, admins_path)
+    admins = [os.path.splitext(file)[0] for file in os.listdir(full_path_admins)]
+
+    return users, admins
 
 
-def prompt_for_login():
-    username = input(f'Welcome to SecuringTheCloud.\n --------------- \n Username:')
+def prompt_for_login(users, admins):
+
+    login_success = False
+
+    while not login_success:
+
+        print('\n')
+
+        privilege_level = None
+        username = input(f'Welcome to SecuringTheCloud. Please log in to proceed.\n--------------- \n Username:\t')
+
+        if username in users:
+            privilege_level = USER_PRIVILEGE
+        elif username in admins:
+            privilege_level = ADMIN_PRIVILEGE
+
+        if privilege_level is not None:
+            password = input(f' Password:\t')
+
+            # get password
+            if privilege_level == USER_PRIVILEGE:
+                expected_pw = get_password(username, USERS_PATH)
+            else:
+                expected_pw = get_password(username, ADMINS_PATH)
+
+            if password == expected_pw:
+                login_success = True
+                return username, privilege_level
+            else:
+                print(f'Incorrect username or password. Please try again.')
+        else:
+            print(f'No user \'{username}\' found. Please try again.')
 
     return None, None
+
+
+def get_password(username, path):
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    full_path = os.path.join(cur_dir, path)
+    files = os.listdir(full_path)
+    files = [os.path.join(full_path, file).rstrip("/").rstrip("\\") for file in files]
+
+    for file in files:
+
+        username_from_path = os.path.splitext(os.path.split(file)[1])[0]
+        if username_from_path == username:
+            with open(file, 'r') as cur_user_file:
+                user_file_contents = cur_user_file.read().splitlines()
+                return user_file_contents[0]
+
+    return None
 
 
 if __name__ == '__main__':
